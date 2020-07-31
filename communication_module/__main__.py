1d34206956f75014d71093d3ea4e63cc4484bd7d
Leonardo de Araújo <dearaujo@uni-bremen.de>
2020-07-31 23:42:27 +0200

    init msg

--- communication_module/__main__.py
+++ communication_module/__main__.py
@@ -6,7 +6,7 @@ from communication_module import encoder
 
 
 def main():
-    print('communication-service-component ' + os.environ['INIT'])
+    print('communication-service-component v1 ' + os.environ['INIT'])
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Communication.ai API'}, pythonic_params=True)
